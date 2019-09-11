ac0439e7f1d8129f870651f993e2505e1a50ae1a
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-12 00:47:48 +0530

    Add the Memoji component to display a cartoon version of myself

---
+++ src/components/Memoji/Memoji.vue
@@ -0,0 +1,55 @@
+<template>
+  <img class="memoji"
+       :class="memojiClasses"
+       alt="Dhruv Bhanushali"
+       src="@/assets/memoji.png"
+       title="That's what I look like, except real-er.">
+</template>
+
+<script>
+  /**
+   * This component displays my memoji in various styles.
+   */
+  export default {
+    name: 'Memoji',
+    props: {
+      /**
+       * _whether or not to bobble my memoji_
+       */
+      isAnimated: {
+        type: Boolean,
+        default: false
+      },
+      /**
+       * _the size of the memoji to show_
+       */
+      size: {
+        type: String,
+        validator: val => [
+          'small',
+          'smaller',
+          'smallest'
+        ].includes(val)
+      }
+    },
+    computed: {
+      /**
+       * _the classes to use on the memoji_
+       */
+      memojiClasses: function () {
+        let classes = [
+          {
+            'animated': this.isAnimated
+          }
+        ]
+        if (this.size) {
+          classes.push(`${this.size}-sized`)
+        }
+        return classes
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./Memoji.styl">
+</style>
