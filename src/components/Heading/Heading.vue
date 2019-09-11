5fbab4804f360d92515dae1de7d2ae58a7142fa2
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-11 23:35:36 +0530

    Create the Heading component

---
+++ src/components/Heading/Heading.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    :is="tag"
+    class="heading"
+    :class="headingClasses">
+    <!-- @slot Content goes here -->
+    <slot/>
+  </div>
+</template>
+
+<script>
+  import Colored from '@/mixins/colored'
+
+  /**
+   * This component displays a heading on the page.
+   */
+  export default {
+    name: 'Heading',
+    mixins: [
+      Colored
+    ],
+    props: {
+      /**
+       * _the prominence of the heading in the page hierarchy_
+       *
+       * level ⩾ 1 and level ⩽ 6
+       * @default 6
+       */
+      level: {
+        type: Number,
+        default: 6,
+        validator: val => val >= 1 && val <= 6
+      }
+    },
+    computed: {
+      /**
+       * _the heading tag from `h1` to `h6` to use for the component_
+       */
+      tag: function () {
+        return `h${this.level}`
+      },
+      /**
+       * _the classes to use on the heading_
+       */
+      headingClasses: function () {
+        return [
+          ...this.coloredClasses
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./Heading.styl">
+</style>
