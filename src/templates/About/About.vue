9cbb154ec40c18d751248f869847460effbf7444
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-12 00:54:25 +0530

    Add an About page to talk about me

---
+++ src/templates/About/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <!-- TODO Put actual content -->
+
+    <Hello message="About"/>
+    <Memoji
+      size="small"
+      is-animated/>
+  </div>
+</template>
+
+<script>
+  import Hello from '@/components/Hello/Hello'
+  import Memoji from '@/components/Memoji/Memoji'
+
+  /**
+   * This page describes me in deep, verging on excruciating, detail.
+   */
+  export default {
+    name: 'About',
+    components: {
+      Memoji,
+      Hello
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./About.styl">
+</style>
