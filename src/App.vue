93a41f8fcd004980f94f1c6b754189af9d43db34
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-12 01:33:56 +0530

    Configure Vue Router and add routing to the main App

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,59 @@
 <template>
-  <div id="app">
-    <!--
-      <img alt="Vue logo" src="@/assets/face_normal.png">
-      <img alt="Vue logo" src="@/assets/face_hat.png">
-    -->
-
-    <Hello/>
+  <div
+    :class="appClasses"
+    id="app">
+    <div class="navigation">
+      <nav>
+        <RouterLink
+          exact
+          active-class="active"
+          to="/">Home
+        </RouterLink>
+        <RouterLink
+          active-class="active"
+          to="/about">About
+        </RouterLink>
+      </nav>
+      <Themer :theme.sync="theme"/>
+    </div>
+    <RouterView/>
   </div>
 </template>
 
 <script>
-  import Hello from '@/components/Hello'
+  import Themer from '@/components/Themer/Themer'
 
+  /**
+   * This is the root level App component.
+   */
   export default {
-    name: 'app',
+    name: 'App',
     components: {
-      Hello
+      Themer
+    },
+    data: function () {
+      return {
+        theme: 'dark'
+      }
+    },
+    computed: {
+      /**
+       * _the classes to apply on the entire #app div_
+       */
+      appClasses: function () {
+        return [
+          `${this.theme}-themed`
+        ]
+      }
+    },
+    mounted: function () {
+      if (localStorage.theme) {
+        // Switch to last used theme
+        this.theme = localStorage.theme
+      }
     }
   }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" src="./App.styl">
 </style>
