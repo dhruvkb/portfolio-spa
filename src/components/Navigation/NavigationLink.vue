dea2b99dfbbdeb7598f9f41448060be5f4f11fdb
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-12 20:16:58 +0530

    Create the NavigationLink component to wrap RouterLink

---
+++ src/components/Navigation/NavigationLink.vue
@@ -0,0 +1,48 @@
+<template>
+  <RouterLink
+    :exact="link.isExact"
+    :to="link.to">
+    <!-- Content goes here -->
+    <slot>
+      <span class="text">
+        {{ link.text }}
+      </span>
+      <FontAwesomeIcon
+        class="icon"
+        :icon="['fas', link.icon]"
+        fixed-width
+        :title="link.text"/>
+    </slot>
+  </RouterLink>
+</template>
+
+<script>
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  /**
+   * This component is one link in a navbar.
+   */
+  export default {
+    name: 'NavigationLink',
+    components: {
+      FontAwesomeIcon
+    },
+    props: {
+      /**
+       * _the attributes of the link, encapsulated in an object_
+       *
+       * Keys ∈ {`'to'`, `'isExact'`, `'text'`, `'icon'`}
+       */
+      link: {
+        type: Object,
+        required: true,
+        validator: val => ['to', 'isExact', 'text', 'icon'].every(
+          key => val.hasOwnProperty(key)
+        )
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./NavigationLink.styl">
+</style>
