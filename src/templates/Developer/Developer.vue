be25446efbca7b9ab9d9a5e7439d7f2d5978b5a5
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-12 13:54:59 +0530

    Create dummy pages for my portfolios

---
+++ src/templates/Developer/Developer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="developer">
+    <Hello message="Hello user!"/>
+  </div>
+</template>
+
+<script>
+  import Hello from '@/components/Hello/Hello'
+
+  export default {
+    name: 'Developer',
+    components: {
+      Hello
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./Developer.styl">
+</style>
---
+++ src/templates/Writer/Writer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="writer">
+    <Hello message="Hello reader!"/>
+  </div>
+</template>
+
+<script>
+  import Hello from '@/components/Hello/Hello'
+
+  export default {
+    name: 'Writer',
+    components: {
+      Hello
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./Writer.styl">
+</style>
