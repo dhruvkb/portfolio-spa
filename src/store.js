927ce05834f4e706621e444ff1b85a62f574541c
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-19 00:52:21 +0530

    Create a Vuex store with a terminal module

--- src/store.js
+++ src/store.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import terminal from './store/modules/terminal'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  strict: process.env.NODE_ENV !== 'production',
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    terminal
   }
 })
---
+++ src/store/modules/terminal.js
@@ -0,0 +1,116 @@
+import { generateTree } from '@/data/portfolio/tree'
+
+import { mapping } from '@/components/Terminal/commands'
+
+const state = {
+  tree: null,
+  currentNode: null,
+  commandHistory: []
+}
+
+const getters = {
+  nodeNamed: function (state) {
+    return function (nodeName) {
+      let nodeInQuestion = null
+
+      state.tree.traverseBreadthFirst(node => {
+        if (node.name === nodeName || node.alternativeName === nodeName) {
+          nodeInQuestion = node
+        }
+      })
+
+      return nodeInQuestion
+    }
+  },
+  nodeLocatedAt: function (state) {
+    return function (path) {
+      let nodeInQuestion = state.currentNode
+      let pathEntities = path.split('/')
+
+      for (let i = 0; i < pathEntities.length; i++) {
+        let entity = pathEntities[i]
+
+        if (entity === '~') {
+          nodeInQuestion = state.tree.root
+        } else if (entity === '.') {
+          // Do nothing as . refers to current directory
+        } else if (entity === '..') {
+          nodeInQuestion = nodeInQuestion.parent
+        } else {
+          let nextNode
+          for (let j = 0; j < nodeInQuestion.children.length; j++) {
+            let child = nodeInQuestion.children[j]
+            if (child.name === entity || child.alternativeName === entity) {
+              nextNode = child
+            }
+          }
+          if (nextNode === undefined) {
+            return null
+          } else {
+            nodeInQuestion = nextNode
+          }
+        }
+      }
+
+      return nodeInQuestion
+    }
+  },
+  absolutePathTo: function (state, getters) {
+    return function (node) {
+      if (node.name === '~') {
+        return '~'
+      } else {
+        return `${getters.absolutePathTo(node.parent)}/${node.name}`
+      }
+    }
+  }
+}
+
+const actions = {}
+
+const mutations = {
+  resetState: function (state) {
+    state.tree = null
+    state.currentNode = null
+    state.commandHistory = []
+  },
+  setTree: function (state, payload) {
+    state.tree = generateTree(payload.fs)
+    state.currentNode = state.tree.root
+  },
+  setCurrentNode: function (state, payload) {
+    state.currentNode = payload.currentNode
+  },
+  runCommand: function (state, payload) {
+    let command = payload.command
+
+    let directory = state.currentNode.name
+
+    let bins = Object.keys(mapping)
+    let [bin, ...args] = command.split(' ')
+    let output = {}
+    if (bins.includes(bin)) {
+      output.component = mapping[bin].component
+      output.args = args
+    } else {
+      output.component = mapping.bad.component
+      output.args = [bin]
+    }
+
+    state.commandHistory.push(
+      {
+        command,
+        directory,
+        output
+      }
+    )
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
