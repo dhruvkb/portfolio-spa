c8b7b28504fccf93a348289731da079e58949e19
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-22 19:59:39 +0530

    Create a new component Indicator that indicates that a page may be scrolled

---
+++ src/components/Indicator/Indicator.vue
@@ -0,0 +1,34 @@
+<template>
+  <FontAwesomeLayers class="indicator">
+    <FontAwesomeIcon
+      class="direction"
+      :icon="['fas', 'angle-down']"
+      fixed-width/>
+    <FontAwesomeIcon
+      class="direction delayed"
+      :icon="['fas', 'angle-down']"
+      fixed-width/>
+  </FontAwesomeLayers>
+</template>
+
+<script>
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+  import {
+    FontAwesomeIcon,
+    FontAwesomeLayers
+  } from '@fortawesome/vue-fontawesome'
+
+  library.add(faAngleDown)
+
+  export default {
+    name: 'Indicator',
+    components: {
+      FontAwesomeIcon,
+      FontAwesomeLayers
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./Indicator.styl">
+</style>
