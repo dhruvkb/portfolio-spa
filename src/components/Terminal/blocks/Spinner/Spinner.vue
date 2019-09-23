40b781afcad0ca384d2f222519c74efec8f641e8
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-23 17:20:32 +0530

    Create a spinner component to indicate long running tasks

---
+++ src/components/Terminal/blocks/Spinner/Spinner.vue
@@ -0,0 +1,36 @@
+<template>
+  <span class="spinner green-colored">{{ spinnerStrings[index] }}</span>
+</template>
+
+<script>
+  /**
+   * This component prints a spinner that is composed of Unicode characters,
+   * as would be seen in a terminal emulator.
+   */
+  export default {
+    name: 'Spinner',
+    data () {
+      return {
+        spinnerStrings: [
+          '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽', '⣾'
+        ],
+        index: 0,
+        interval: 0.1,
+        updateLooper: null
+      }
+    },
+    methods: {
+      startLooping () {
+        this.updateLooper = setInterval(() => {
+          this.index = ++this.index % this.spinnerStrings.length
+        }, this.interval * 1000)
+      }
+    },
+    created () {
+      this.startLooping()
+    },
+    beforeDestroy () {
+      clearInterval(this.updateLooper)
+    }
+  }
+</script>
