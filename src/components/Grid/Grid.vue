28bf3b9f7221d1874523bfe9add1becf3e7bf4e6
Dhruv Bhanushali <dhruv_b@live.com>
2019-09-27 13:01:03 +0530

    Create a component for designing grid layouts

---
+++ src/components/Grid/Grid.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid">
+    <!-- @slot `GridCell` components go here -->
+    <slot/>
+  </div>
+</template>
+
+<script>
+  /**
+   * This component organises content on a 2D surface.
+   */
+  export default {
+    name: 'Grid'
+  }
+</script>
+
+<style scoped lang="stylus" src="./Grid.styl">
+</style>
