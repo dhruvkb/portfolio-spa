287cecdb78f193a962b19b4b0cbd1608d9c0ec4d
Dhruv Bhanushali <dhruv_b@live.com>
2019-10-22 00:54:35 +0530

    Create a Spinner element in Blog

---
+++ src/templates/Blog/elements/Spinner/Spinner.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="spinner"><FontAwesomeIcon
+    :icon="['fas', 'i-cursor']"
+    fixed-width/></span>
+</template>
+
+<script>
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faICursor } from '@fortawesome/free-solid-svg-icons'
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  library.add(faICursor)
+
+  /**
+   * Indicates loading status
+   */
+  export default {
+    name: 'Spinner',
+    components: {
+      FontAwesomeIcon
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./Spinner.styl">
+</style>
