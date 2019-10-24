54da179b340106cfafce88fffec7ddb4abd70bd3
Dhruv Bhanushali <dhruv_b@live.com>
2019-10-24 17:05:23 +0530

    Configure Babel from Prism

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,24 @@ module.exports = {
   presets: [
     '@vue/app',
     '@vue/babel-preset-jsx'
+  ],
+  plugins: [
+    [
+      'prismjs',
+      {
+        languages: [
+          'java',
+          'javascript',
+          'latex',
+          'markdown',
+          'python',
+          'rest',
+          'yaml'
+        ],
+        plugins: ['autolinker'],
+        theme: 'solarizedlight',
+        css: true
+      }
+    ]
   ]
 }
