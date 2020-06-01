ca3c542e9563c764490f1ee3c32e803301942f3e
Dhruv Bhanushali <dhruv_b@live.com>
2020-06-01 07:46:26 +0530

    Refactor Cell for better code quality

--- src/components/grid/pieces/cell/Cell.vue
+++ src/components/grid/pieces/cell/Cell.vue
@@ -10,13 +10,13 @@
    * This component is one cell in a grid layout.
    */
   export default {
-    name: 'GridCell',
+    name: 'Cell', // Always use as Grid.Cell
     props: {
       /**
        * _the number of slices this column should span on different devices_
        *
-       * Note that in a 12-slice grid, number of columns that can be
-       * accommodated is `12 / span`.
+       * Note that in a 12-slice grid, the number of columns that can be
+       * accommodated is 12 / span.
        *
        * Other than desktop, all values have a default and will be resolved as
        * follows. On phones and tablets in portrait orientation, the cell
@@ -29,9 +29,9 @@
        * ```
        * [desktop]
        * [phone, desktop]
-       * [phone, desktop, big desktop]
-       * [phone, tablet, desktop, big desktop]
-       * [phone, tablet-portrait, tablet-landscape, desktop, big-desktop]
+       * [phone, desktop-standard, desktop-big]
+       * [phone, tablet, desktop-standard, desktop-big]
+       * [phone, tablet-portrait, tablet-landscape, desktop-standard, desktop-big]
        * ```
        */
       spanSet: {
@@ -43,18 +43,40 @@
       }
     },
     computed: {
-      spans: function () {
-        let phone = null
-        let tablet = null
-        let tabletPortrait = null
-        let tabletLandscape = null
-        let desktop = null
-        let bigDesktop = null
+      /**
+       * Get the classes to use on the grid cell.
+       * @returns {Array} an array of all the classes to apply on the element
+       */
+      cellClasses: function () {
+        return [
+          `phone-${this.spans.phone}-wide`,
+          `tablet-portrait-${this.spans.tabletPortrait}-wide`,
+          `tablet-landscape-${this.spans.tabletLandscape}-wide`,
+          `desktop-standard-${this.spans.desktopStandard}-wide`,
+          `desktop-big-${this.spans.desktopBig}-wide`
+        ]
+      },
 
+      /**
+       * Get the number of columns to span for each device category.
+       * @return {Object} a mapping of device category and column spans
+       */
+      spans: function () {
         const spanSet = this.spanSet
         const count = spanSet.length
 
-        // Prop processing
+        // Start with null
+        let phone = null
+
+        let tablet = null
+        let tabletPortrait = null
+        let tabletLandscape = null
+
+        let desktop = null
+        let desktopStandard = null
+        let desktopBig = null
+
+        // Assign list items
         switch (count) {
           case 1:
             [
@@ -70,16 +92,16 @@
           case 3:
             [
               phone,
-              desktop,
-              bigDesktop
+              desktopStandard,
+              desktopBig
             ] = spanSet
             break
           case 4:
             [
               phone,
               tablet,
-              desktop,
-              bigDesktop
+              desktopStandard,
+              desktopBig
             ] = spanSet
             break
           case 5:
@@ -87,34 +109,28 @@
               phone,
               tabletPortrait,
               tabletLandscape,
-              desktop,
-              bigDesktop
+              desktopStandard,
+              desktopBig
             ] = spanSet
             break
         }
 
-        // Defaults
+        // Fall back for empty values
         phone = phone || 12
+
         tabletPortrait = tabletPortrait || tablet || phone
         tabletLandscape = tabletLandscape || tablet || desktop
-        bigDesktop = bigDesktop || desktop
+
+        desktopStandard = desktopStandard || desktop
+        desktopBig = desktopBig || desktop
 
         return {
           phone,
           tabletPortrait,
           tabletLandscape,
-          desktop,
-          bigDesktop
+          desktopStandard,
+          desktopBig
         }
-      },
-      cellClasses: function () {
-        return [
-          `phone-${this.spans.phone}-wide`,
-          `tablet-portrait-${this.spans.tabletPortrait}-wide`,
-          `tablet-landscape-${this.spans.tabletLandscape}-wide`,
-          `desktop-standard-${this.spans.desktop}-wide`,
-          `desktop-big-${this.spans.bigDesktop}-wide`
-        ]
       }
     }
   }
