b5278cf7ee0ce23754f1a83da6db0f4f2ec48bd4
Dhruv Bhanushali <dhruv_b@live.com>
2020-06-04 04:11:33 +0530

    Create a new component that links to the portfolio source code

---
+++ src/templates/home/components/source/Source.vue
@@ -0,0 +1,32 @@
+<template>
+  <a
+    v-shortkey="['s']"
+    tabindex="0"
+    class="source"
+    href="https://github.com/dhruvkb/portfolio/"
+    target="_blank"
+    title="[S] See the source code.">
+    <FontAwesomeIcon
+      class="icon"
+      :icon="['fab', 'github']"
+      fixed-width/>
+  </a>
+</template>
+
+<script>
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faGithub } from '@fortawesome/free-brands-svg-icons'
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  library.add(faGithub)
+
+  export default {
+    name: 'Source',
+    components: {
+      FontAwesomeIcon
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./Source.scss">
+</style>
