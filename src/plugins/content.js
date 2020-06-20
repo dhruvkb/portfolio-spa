934229517a308fd0846f2c24c7b4d25a0d446cc7
Dhruv Bhanushali <dhruv_b@live.com>
2020-06-20 14:48:28 +0530

    Cleanup the content mixin and remove the plugin

--- src/plugins/content.js
+++
@@ -1,5 +0,0 @@
-export default {
-  install (Vue, options) {
-    Vue.prototype.$headSpanSet = [12, 6]
-  }
-}
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -4,10 +4,8 @@ import VueModulo from '@/plugins/modulo'
 import VueSolarized from '@/plugins/solarized'
 import VueNavigation from '@/plugins/navigation'
 import VueRole from '@/plugins/role'
-import VueContent from '@/plugins/content'
 
 Vue.use(VueModulo)
 Vue.use(VueSolarized)
 Vue.use(VueNavigation)
 Vue.use(VueRole)
-Vue.use(VueContent)
