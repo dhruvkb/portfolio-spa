64e6112ac21e66a6232ccc823ca032e32c4c427f
Dhruv Bhanushali <dhruv_b@live.com>
2020-06-20 09:51:53 +0530

    Extend grid to have two dimensional row and column count

--- src/components/grid/Grid.vue
+++ src/components/grid/Grid.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="grid">
+  <div
+    class="grid"
+    :style="gridStyles">
     <!-- @slot `Grid.Cell` components go here -->
     <slot/>
   </div>
@@ -13,6 +15,34 @@
    */
   export default {
     name: 'Grid',
+    props: {
+      /**
+       * _the number of columns to have on the grid_
+       */
+      columnCount: {
+        type: Number,
+        default: 12
+      },
+      /**
+       * _the number of rows to have on the grid_
+       */
+      rowCount: {
+        type: Number,
+        default: 1
+      }
+    },
+    computed: {
+      /**
+       * Get the styles to apply on the grid.
+       * @returns {Object} an mapping of CSS properties and values to apply on the element
+       */
+      gridStyles () {
+        return {
+          '--column-count': this.columnCount,
+          '--row-count': this.rowCount
+        }
+      }
+    },
 
     // Child components
     Cell
