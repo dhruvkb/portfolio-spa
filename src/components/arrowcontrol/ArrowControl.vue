314bf0272a459f831632cd4f8a7b9fcc5248b796
Dhruv Bhanushali <dhruv_b@live.com>
2020-06-26 23:46:15 +0530

    Create an arrow control to unify common functionality

---
+++ src/components/arrowcontrol/ArrowControl.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="arrow-control" :class="arrowLinkClasses">
+    <div class="text">
+      <!-- @slot Main text goes here -->
+      <slot/>
+    </div>
+    <div class="punctuation">
+      <!-- @slot Punctuation following the text goes here -->
+      <slot name="punctuation"/>
+    </div>
+    <div class="arrow">
+      <FontAwesomeIcon
+        :icon="['fas', 'arrow-right']"
+        fixed-width/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+  import focusable from '@/mixins/focusable'
+
+  library.add(faArrowRight)
+
+  export default {
+    name: 'ArrowControl',
+    mixins: [
+      focusable
+    ],
+    components: {
+      FontAwesomeIcon
+    },
+    computed: {
+      /**
+       * Get the classes to use on the arrow control.
+       * @returns {Array} an array of all the classes to apply on the element
+       */
+      arrowLinkClasses () {
+        return [
+          {
+            ...this.focusableClasses
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./ArrowControl.scss">
+</style>
