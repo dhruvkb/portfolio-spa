24da6d06ee9dbf6c24259c0706655e28007df5af
Dhruv Bhanushali <dhruv_b@live.com>
2020-06-27 00:23:32 +0530

    Create a component to fetch more posts for the blog

---
+++ src/templates/blog/subpages/home/components/more/More.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    v-if="totalCount > posts.length"
+    class="more">
+    <Spinner v-if="isFetching"/>
+    <button
+      v-else
+      class="load-more"
+      @click="getPosts">
+      <ArrowControl>
+        <template #default>Load more posts</template>
+        <template #punctuation>…</template>
+      </ArrowControl>
+    </button>
+  </div>
+</template>
+
+<script>
+  import { mapActions, mapState } from 'vuex'
+
+  import ArrowControl from '@/components/arrowcontrol/ArrowControl'
+
+  import { Blog } from '@/templates/blog/Blog'
+
+  export default {
+    name: 'More',
+    components: {
+      ArrowControl,
+
+      Spinner: Blog.Spinner
+    },
+    computed: {
+      ...mapState('blog', [
+        'totalCount',
+        'posts',
+        'isFetching'
+      ])
+    },
+    methods: {
+      ...mapActions('blog', [
+        'getPosts'
+      ])
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./More.scss">
+</style>
