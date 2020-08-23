5493f4e45752280166f08f6c51b96ec6429d4a71
Dhruv Bhanushali <dhruv_b@live.com>
2020-08-23 09:25:56 +0530

    Configure the app into a PWA

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,44 @@ module.exports = {
       })
   },
 
+  pwa: {
+    name: '@dhruvkb',
+    themeColor: '#073642',
+    msTileColor: '#073642',
+    iconPaths: {
+      favicon32: 'favicon/favicon-32x32.png',
+      favicon16: 'favicon/favicon-16x16.png',
+      appleTouchIcon: 'favicon/apple-touch-icon-180x180.png',
+      maskIcon: 'favicon/safari-pinned-tab.svg',
+      msTileImage: 'favicon/mstile-144x144.png'
+    },
+    manifestOptions: {
+      short_name: '@dhruvkb',
+      name: '@dhruvkb',
+      description: 'Software engineer & other things',
+      themeColor: '#073642',
+      background_color: '#073642',
+      icons: [192, 512].map(size => [true, false].map(isMaskable => {
+        let suffix = ''
+        let purpose = 'any'
+        if (isMaskable) {
+          suffix = '-maskable'
+          purpose = 'maskable'
+        }
+        return {
+          src: `favicon/android-chrome${suffix}-${size}x${size}.png`,
+          type: 'image/png',
+          sizes: `${size}x${size}`,
+          purpose: purpose
+        }
+      })).flat(),
+      shortcuts: ['Portfolio', 'About', 'Contact'].map(link => ({
+        name: link,
+        url: `/#/${link.toLocaleLowerCase()}`
+      }))
+    }
+  },
+
   pluginOptions: {
     i18n: {
       locale: 'en',
