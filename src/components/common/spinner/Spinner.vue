3be9106fc84ed3d1b0be0d91cf7c6d54684fc05c
Dhruv Bhanushali <dhruv_b@live.com>
2020-08-25 19:19:19 +0530

    Add some snazziness to the spinner

--- src/components/common/spinner/Spinner.vue
+++ src/components/common/spinner/Spinner.vue
@@ -6,19 +6,17 @@
     width="38"
     height="38"
     viewBox="0 0 38 38">
-    <g fill="none" fill-rule="evenodd">
-      <g transform="translate(1 1)" stroke-width="2">
-        <circle class="track" cx="18" cy="18" r="18"/>
-        <path d="M36 18c0-9.94-8.06-18-18-18">
-          <animateTransform
-            attributeName="transform"
-            type="rotate"
-            from="0 18 18"
-            to="360 18 18"
-            dur="1s"
-            repeatCount="indefinite"/>
-        </path>
-      </g>
+    <defs>
+      <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+        <stop stop-color="var(--spinner-color)" offset="0%" stop-opacity="0" />
+        <stop stop-color="var(--spinner-color)" offset="66.667%" stop-opacity=".631"/>
+        <stop stop-color="var(--spinner-color)" offset="100%"/>
+      </linearGradient>
+    </defs>
+    <g transform="translate(1 1)" fill-rule="evenodd">
+      <circle class="track" cx="18" cy="18" r="18"/>
+      <path class="runner tail" d="M36 18c0-9.94-8.06-18-18-18" stroke="url(#a)"/>
+      <circle class="runner head" cx="36" cy="18" r="1"/>
     </g>
   </svg>
 </template>
@@ -26,12 +24,19 @@
 <script>
   import colored from '@/mixins/colored'
 
+  /**
+   * This is the beautiful spinner that should be used everywhere.
+   */
   export default {
     name: 'Spinner',
     mixins: [
       colored
     ],
     computed: {
+      /**
+       * Get the classes to use on the spinner.
+       * @returns {Array} an array of all the classes to apply on the element
+       */
       spinnerClasses () {
         return [
           ...this.coloredClasses
