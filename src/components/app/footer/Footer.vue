e9f1ffdd68b00ba21400681d34947e1f7a6f51d1
Dhruv Bhanushali <dhruv_b@live.com>
2020-08-30 13:36:56 +0530

    Use footer for phone navigation as it is easier to reach

---
+++ src/components/app/footer/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <Navigation is-phone/>
+  </footer>
+</template>
+
+<script>
+  import Navigation from '@/components/app/navigation/Navigation'
+
+  export default {
+    name: 'Footer',
+    components: {
+      Navigation
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./Footer.scss"/>
